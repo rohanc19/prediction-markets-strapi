689b29a930189844a3e894f3e4ede6aa191585b6
rohanc19 <1rn21ai098.rohan@gmail.com>
2025-05-13 07:15:44 +0530

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'your-secret-here'),
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'your-salt-here'),
+    salt: env('API_TOKEN_SALT'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT', 'your-salt-here'),
+      salt: env('TRANSFER_TOKEN_SALT', env('API_TOKEN_SALT')),
     },
   },
 });
